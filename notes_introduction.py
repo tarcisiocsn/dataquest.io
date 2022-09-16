567ed9eed856ac8cef46e30a844c7e0839b9e568
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2022-09-16 19:08:33 -0400

    Update notes_introduction.py

--- notes_introduction.py
+++ notes_introduction.py
@@ -998,4 +998,33 @@ for row in moma:
     if not nationality:
         nationality = 'Nationality Unknown'
     row[2] = nationality
+    
+# create a function to change the strings in a list
+test_data = ["1912", "1929", "1913-1923",
+             "(1951)", "1994", "1934",
+             "c. 1915", "1995", "c. 1912",
+             "(1988)", "2002", "1957-1959",
+             "c. 1955.", "c. 1970's", 
+             "C. 1990-1999"]
+
+bad_chars = ["(",")","c","C",".","s","'", " "]
+
+# replace the bad characters (char)
+def strip_characters(string):
+    for char in bad_chars:
+        string = string.replace(char, "")
+    return string 
+
+# insert it in a new list
+
+stripped_test_data = []
+
+for s in test_data:
+    s = strip_characters(s)
+    stripped_test_data.append(s)
+print(stripped_test_data)
+
+
+    
+   
 
